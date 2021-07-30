f2ba73eaabdb3e879fee9a369281b9410b7c089e
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-07-29 23:48:56 -0300

    modificando o @RequestMapping

--- src/main/java/com/back/ofertaBack/controller/RequestController.java
+++ src/main/java/com/back/ofertaBack/controller/RequestController.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ofertaPublica/back")
+@RequestMapping("/api/ofertaPublica/back")
 public class RequestController {
 
     private RequestService requestService;
